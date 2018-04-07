c0e5eb429e7947d22d30ad9d98029afd1aca8879
Oleksandr LEVCHENKOV <ollevche@e1r7p1.unit.ua>
2018-04-07 16:41:00 +0300

    added skip_lines()

--- library/sources/skip_lines.c
+++ library/sources/skip_lines.c
@@ -1,31 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   skip_lines.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ollevche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/12 12:37:36 by ollevche          #+#    #+#             */
-/*   Updated: 2018/01/12 13:16:01 by ollevche         ###   ########.fr       */
+/*   Created: 2018/04/07 16:33:17 by ollevche          #+#    #+#             */
+/*   Updated: 2018/04/07 16:33:18 by ollevche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# include "libft.h"
-
-# define BUFF_SIZE 1024
-
-typedef struct	s_file
+int	skip_lines(int fd, int amount)
 {
-	int					fd;
-	char				*buf;
-	struct s_file		*next;
-}				t_file;
+	int		i;
+	char	*line;
 
-int				get_next_line(const int fd, char **line);
-char			*safe_gnl(int fd);
-int				skip_lines(int fd, int amount);
-
-#endif
+	i = 0;
+	while (i < amount)
+	{
+		line = safe_gnl(fd);
+		if (!line)
+			return (-1);
+		free(line);
+		i++;
+	}
+	return (1);
+}
