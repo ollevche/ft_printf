085d3e581897a005cddec1413a7c0854fcd3c01e
Oleksandr LEVCHENKOV <ollevche@student.unit.ua>
2018-09-25 19:10:20 +0300

    simplified

--- sources/libft-gnl/ft_strndup.c
+++ sources/libft-gnl/ft_strndup.c
@@ -15,12 +15,8 @@
 char	*ft_strndup(const char *src, size_t n)
 {
 	char	*res;
-	size_t	size;
 
 	if (!src || !(res = ft_strnew(n)))
 		return (NULL);
-	size = ft_strlen(src);
-	if (size > n)
-		size = n;
 	return (ft_strncpy(res, src, n));
 }
